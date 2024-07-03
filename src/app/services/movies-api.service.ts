f7c6c7e860a842dbd9ac543caea9ad9a9b52face
Mahroof_Rufi <135498710+Mahroof-Rufi@users.noreply.github.com>
2024-07-03 20:14:26 +0530

    environment added

--- src/app/services/movies-api.service.ts
+++ src/app/services/movies-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -9,72 +10,69 @@ export class MoviesAPIService {
 
   constructor(private http:HttpClient) { }
 
-  baseurl = "https://api.themoviedb.org/3";
-  apikey = "277134d7036690fe2d6692daac8acbea";
-
   //get banner data
   bannerApiData():Observable<any> {
-    return this.http.get(`${this.baseurl}/trending/all/week?api_key=${this.apikey}`)
+    return this.http.get(`${environment.baseURL}/trending/all/week?api_key=${environment.apiKey}`)
   }
 
   //get movie details
   getMovieDetails(id:any):Observable<any> {
-    return this.http.get(`${this.baseurl}/movie/${id}?api_key=${this.apikey}`);
+    return this.http.get(`${environment.baseURL}/movie/${id}?api_key=${environment.apiKey}`);
   }
 
   // get movie cast details
   getCastDetails(id:any):Observable<any> {
-    return this.http.get(`${this.baseurl}/movie/${id}/credits?api_key=${this.apikey}`);
+    return this.http.get(`${environment.baseURL}/movie/${id}/credits?api_key=${environment.apiKey}`);
   }
 
   // get movie trailer
   getMovieTrailer(id:any):Observable<any> {
-    return this.http.get(`${this.baseurl}/movie/${id}/videos?api_key=${this.apikey}`);
+    return this.http.get(`${environment.baseURL}/movie/${id}/videos?api_key=${environment.apiKey}`);
   }
 
   // fetch movies category base
   // get trending movies data
   trendingMoviesData():Observable<any> {
-    return this.http.get(`${this.baseurl}/trending/movie/day?api_key=${this.apikey}`);
+    return this.http.get(`${environment.baseURL}/trending/movie/day?api_key=${environment.apiKey}`);
   }
 
   // get action movies data
   actionMoviesData():Observable<any> {
-    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=28`);
+    return this.http.get(`${environment.baseURL}/discover/movie?api_key=${environment.apiKey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=28`);
   }
 
   // get romance movies data
   romanceMoviesData():Observable<any> {
-    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=10749`);
+    return this.http.get(`${environment.baseURL}/discover/movie?api_key=${environment.apiKey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=10749`);
   }
 
   // get adventure movies data
   adventureMoviesData():Observable<any> {
-    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=12`);
+    return this.http.get(`${environment.baseURL}/discover/movie?api_key=${environment.apiKey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=12`);
   }
 
   // get animation movies data
   animationMoviesData():Observable<any> {
-    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=16`);
+    return this.http.get(`${environment.baseURL}/discover/movie?api_key=${environment.apiKey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=16`);
   }
 
   // get comedy movies data
   comedyMoviesData():Observable<any> {
-    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=35`);
+    return this.http.get(`${environment.baseURL}/discover/movie?api_key=${environment.apiKey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=35`);
   }
 
   // get documentary movies data
   documentaryMoviesData():Observable<any> {
-    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=99`);
+    return this.http.get(`${environment.baseURL}/discover/movie?api_key=${environment.apiKey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=99`);
   }
 
   // get science-fiction movies data
   scienceFictionMoviesData():Observable<any> {
-    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=878`);
+    return this.http.get(`${environment.baseURL}/discover/movie?api_key=${environment.apiKey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=878`);
   }
 
   // get thriller movies data
   thrillerMoviesData():Observable<any> {
-    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=53`);
+    return this.http.get(`${environment.baseURL}/discover/movie?api_key=${environment.apiKey}&language=en-US&sort_by=release_date.desc&page=1&with_genres=53`);
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-          
+    baseURL: "https://api.themoviedb.org/3",
+    apiKey: "277134d7036690fe2d6692daac8acbea",
 };
